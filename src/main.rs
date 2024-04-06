f7b37411b949cf08536f529ef3d99291fa0df5ad
Jesse Alford <jesse@nonlinear.garden>
2024-04-06 16:41:54 -0700

    Just typed in through page 54

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use bracket_lib::prelude::*;
+
+fn main() -> BError {
+    enum GameMode {
+        Menu,
+        Playing,
+        End,
+    }
+    struct State {
+        mode: GameMode,
+    }
+    impl State {
+        fn new() ->  Self {
+            State{
+            mode: GameMode::Menu,
+            }
+        }
+    }
+    impl GameState for State {
+        fn tick(&mut self, ctx: &mut BTerm) {
+            ctx.cls();
+            ctx.print(1, 1, "Hello, Bracket Terminal!");
+        }
+    }
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+    main_loop(context, State::new())
+}
