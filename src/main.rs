51575ddeefe93c619b5aff1a984946d19b6f6057
Jesse Alford <jesse@nonlinear.garden>
2024-04-07 12:33:51 -0700

    Add menu/state loop

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,49 @@ fn main() -> BError {
             mode: GameMode::Menu,
             }
         }
+        fn main_menu (&mut self, ctx: &mut BTerm){
+            ctx.cls();
+            ctx.print(1, 1, "Flappy Dragon v0.0.1");
+            ctx.print_centered(5, "Welcome to Flappy Dragon!");
+            ctx.print_centered(8, "(P) Play Game");
+            ctx.print_centered(9, "(Q) Quit Game");
+            if let Some(key) = ctx.key {
+                match key {
+                    VirtualKeyCode::P => self.restart(),
+                    VirtualKeyCode::Q => ctx.quitting = true,
+                    _ => {}
+                }
+            }
+        }
+        fn dead (&mut self, ctx: &mut BTerm){
+            ctx.cls();
+            ctx.print(1, 1, "Flappy Dragon v0.0.1");
+            ctx.print_centered(5, "You are DEAD!");
+            ctx.print_centered(8, "(P) Play Again");
+            ctx.print_centered(9, "(Q) Quit Game");
+            if let Some(key) = ctx.key {
+                match key {
+                    VirtualKeyCode::P => self.restart(),
+                    VirtualKeyCode::Q => ctx.quitting = true,
+                    _ => {}
+                }
+            }
+        }
+        fn play (&mut self, ctx: &mut BTerm){
+            //TODO: fill in
+            self.mode = GameMode::End;
+        }
+        fn restart(&mut self) {
+            self.mode = GameMode::Playing;
+        }
     }
     impl GameState for State {
         fn tick(&mut self, ctx: &mut BTerm) {
-            ctx.cls();
-            ctx.print(1, 1, "Hello, Bracket Terminal!");
+            match self.mode {
+                GameMode::Menu => self.main_menu(ctx),
+                GameMode::Playing => self.play(ctx),
+                GameMode::End => self.dead(ctx),
+            }
         }
     }
     let context = BTermBuilder::simple80x50()
